f59f6f8178a76486d44288a2715c7cc96a7ade6b
boaicha <95445885+boaicha@users.noreply.github.com>
2024-03-16 15:49:03 +0100

    Update UserControllerTest.php

--- tests/ti/UserControllerTest.php
+++ tests/ti/UserControllerTest.php
@@ -8,20 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    /**
-     * @doesNotPerformAssertions
-     */
-//    public function testAdminLogin(){
-//        $client = static::createClient();
-//        $userRepository = static::getContainer()->get(UserRepository::class);
-//
-//        $user = $userRepository->findOneByEmail("jaja@jaja.com");
-//        $client->loginUser($user);
-//        //$this->assertTrue(true);
-//        return $client;
-//
-//
-//    }
 
     public function testAdminLogin($email = 'jaja@jaja.com')
     {
@@ -47,60 +33,14 @@ class UserControllerTest extends WebTestCase
 
     }
 
-//    public function testCreateAction(){
-//
-//        $client = $this->testAdminLogin();
-//
-//        $create = $client->request('GET', '/users/create');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//
-//        $form = $create->selectButton('Ajouter')->form();
-//        $form['user[username]'] = 'userTest';
-//        $form['user[password][first]'] = 'password';
-//        $form['user[password][second]'] = 'password';
-//        $form['user[email]'] = 'userTest@gmail.com';
-//        $form['user[role]']->select('ROLE_USER');
-//        $client->submit($form);
-//
-////        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-////        $this->assertTrue($client->getResponse()->isRedirect('/users'));
-//        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-//
-//// With this line
-//        $this->assertTrue($client->getResponse()->isRedirect());
-//
-//
-//
-//    }
 
-//    public function testCreateAction(){
-//        $client = $this->testAdminLogin();
-//
-//        $create = $client->request('GET', '/users/create');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//
-//        $form = $create->selectButton('Ajouter')->form();
-//        $form['user[username]'] = 'userTest';
-//        $form['user[password][first]'] = 'password';
-//        $form['user[password][second]'] = 'password';
-//        $form['user[email]'] = 'userTest@gmail.com';
-//        $form['user[role]']->select("ROLE_USER"); // Updated line
-//        $client->submit($form);
-//
-//        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-//        $this->assertTrue($client->getResponse()->isRedirect());
-//    }
 
     public function testCreateAction()
     {
         $client = static::createClient();
-        // Generate a unique email address for each test run
         $email = 'userTest' . uniqid() . '@gmail.com';
         $username = 'userTest' . uniqid();
 
-        // Make sure to replace this with an existing user in your database
         $existingUserEmail = 'jaja@jaja.com';
 
         $userRepository = static::getContainer()->get(UserRepository::class);
@@ -166,39 +106,5 @@ class UserControllerTest extends WebTestCase
 
     }
 
-//    public function testEditAction()
-//    {
-//
-//            self::ensureKernelShutdown();
-//            $client = static::createClient();
-//
-//            // Make sure to replace this with an existing user in your database
-//            $existingUserEmail = 'jaja@jaja.com';
-//
-//            $userRepository = static::getContainer()->get(UserRepository::class);
-//            $existingUser = $userRepository->findOneByEmail($existingUserEmail);
-//
-//            $client->loginUser($existingUser);
-//            $crawler = $client->request('GET', '/users/' . $existingUser->getId() . '/edit');
-//
-//            $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//
-//            // Créez un formulaire de test pour la modification d'un utilisateur et soumettez-le.
-//            $form = $crawler->selectButton('Modifier')->form();
-//            // Modifiez les champs du formulaire selon vos besoins.
-//            $form['user[username]'] = 'utilisateur_modifie';
-//            $form['user[password][first]'] = 'nouveau_mot_de_passe';
-//            $form['user[password][second]'] = 'nouveau_mot_de_passe';
-//            $client->submit($form);
-//
-//            $this->assertTrue($client->getResponse()->isRedirect('/users'));
-//
-//            // Chargez à nouveau l'utilisateur depuis la base de données après la modification.
-//            $updatedUser = $this->getContainer()->get('doctrine')->getRepository(User::class)->find($existingUser->getId());
-//
-//            // Vérifiez que le rôle de l'utilisateur a été modifié correctement.
-//            $this->assertContains(["ROLE_USER"], $updatedUser->getRoles());
-//
-//    }
-
 }
+
